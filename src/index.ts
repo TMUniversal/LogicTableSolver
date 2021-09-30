b96371443c32b9ac30744cfbf9d6d79f99893deb
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2021-09-30 15:08:55 +0200

    feat: working product

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,63 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { generateInputTable } from './generator'
 import { and, eq, not } from './operations'
 
 type claim = (...args: boolean[]) => boolean
 
-function evaluate (size: number, claims: claim[]): boolean[][] {
+export function evaluate (size: number, claims: claim[]): [truthValues: boolean[][], evaluated: boolean[][]] {
   const inputTable = generateInputTable(size)
 
   const output: boolean[][] = []
 
+  let index = 0
   for (const row of inputTable) {
+    output[index] = []
+
     for (const claim of claims) {
-      row.push(claim(...row))
+      output[index].push(eq(row[claims.indexOf(claim)], claim(...row)))
     }
+
+    index++
   }
 
-  return output
+  return [inputTable, output]
 }
 
-function main (): void {
+/*
+  Example Case
+
+  A claims B is lying
+  B claims C is lying
+  C claims A and B are lying
+
+  Solution:
+
+  A and C are lying, B is telling the truth
+*/
+
+export function main (): void {
   const size = 3
   const claims: claim[] = [
-    (a, b) => eq(a, not(b)),
-    (_, b, c) => eq(b, not(c)),
-    (a, b, c) => eq(c, and(not(a), not(b)))
+    (a, b) => not(b),
+    (_, b, c) => not(c),
+    (a, b, c) => and(not(a), not(b))
   ]
 
-  // eslint-disable-next-line no-console
-  console.table(evaluate(size, claims))
+  const [truthValues, result] = evaluate(size, claims)
+
+  console.log('Input Values')
+  console.table(truthValues)
+
+  console.log('Results')
+  console.table(result)
+
+  console.log('Truth Condition(s):')
+
+  // Find combination of inputs that result in all outputs being true
+  result.filter(value => value.every(v => v)).length > 1 && console.log('There are more than one possible answers')
+
+  console.log(truthValues[result.findIndex(value => value.every(v => v))], 'results in all true')
 }
 
 main()
