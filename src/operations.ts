0673be78d22257904181a66a5e613fd2b51430db
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2021-09-30 12:08:01 +0200

    feat: add missing operand

--- src/operations.ts
+++ src/operations.ts
@@ -10,8 +10,12 @@ export function or (a: boolean, b: boolean): boolean {
   return a || b
 }
 
+export function not (a: boolean): boolean {
+  return !a
+}
+
 export function xor (a: boolean, b: boolean): boolean {
-  return !(a === b)
+  return not(a === b)
 }
 
 export function impl (a: boolean, b: boolean): boolean {
